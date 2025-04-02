1e83c0709cf9b8f0f19fe72b7a2806e019a28aab
Jiyong <hickerd@sch.ac.kr>
2025-04-03 01:28:17 +0900

    feat: Implement default components

---
+++ AD-BlockBuster/Source/Common/Component/BaseViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BaseViewController.swift
+//  AD-BlockBuster
+//
+//  Created by 정지용 on 4/1/25.
+//
+
+import UIKit
+
+class BaseViewController<T: BaseView>: UIViewController {
+    // MARK: - Properties
+    let contentView: T
+    
+    // MARK: - Initialize
+    init() {
+        self.contentView = T()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    override func loadView() {
+        if contentView.backgroundColor == nil {
+            contentView.backgroundColor = .white
+        }
+        view = contentView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupView()
+        self.configure()
+        self.setupLayout()
+        self.bindAction()
+        self.bindState()
+    }
+    
+    // MARK: - Common Methods
+    func setupView() {}
+    func configure() {}
+    func setupLayout() {}
+    func bindAction() {}
+    func bindState() {}
+}
