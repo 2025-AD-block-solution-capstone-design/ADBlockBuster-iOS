65d128fb2ffc6fd6e1fe8d0f010e132c99e1105f
Jiyong <hickerd@sch.ac.kr>
2025-04-03 01:24:44 +0900

    feat: Initialize Safari Web Extension & Content Blocker

---
+++ NetworkPrivacyEngine/ContentBlockerRequestHandler.swift
@@ -0,0 +1,22 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  NetworkPrivacyEngine
+//
+//  Created by 정지용 on 4/2/25.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
