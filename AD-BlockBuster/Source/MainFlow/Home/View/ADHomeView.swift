bdf7bf0d0e52251b8593d1957516aad298062a41
Jiyong <hickerd@sch.ac.kr>
2025-04-10 02:26:28 +0900

    feat: Add ADHomeView for AppCoordinator

---
+++ AD-BlockBuster/Source/MainFlow/Home/View/ADHomeView.swift
@@ -0,0 +1,11 @@
+//
+//  ADHomeView.swift
+//  AD-BlockBuster
+//
+//  Created by 정지용 on 4/2/25.
+//
+
+import UIKit
+
+final class ADHomeView: BaseView {
+}
---
+++ AD-BlockBuster/Source/MainFlow/Home/View/ADHomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ADHomeViewController.swift
+//  AD-BlockBuster
+//
+//  Created by 정지용 on 4/1/25.
+//
+
+import UIKit
+
+final class ADHomeViewController: BaseViewController<ADHomeView> {
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+}
