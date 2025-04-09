363a2dd9aa3c7c50477b1f99118e9db6ade87bcb
Jiyong <hickerd@sch.ac.kr>
2025-04-10 02:48:20 +0900

    feat: Initialize UINavigationBarAppearance

--- AD-BlockBuster/Source/App/SceneDelegate.swift
+++ AD-BlockBuster/Source/App/SceneDelegate.swift
@@ -19,6 +19,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        setupNavigationBar()
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
@@ -31,7 +32,29 @@ private extension SceneDelegate {
     func startScene() {
         self.coordinator = AppCoordinator(navigationController: navigationController)
         coordinator?.start()
+    }
+    
+    func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.neutral,
+            .font: UIFont.pretendard(size: 18, weight: .semibold)
+        ]
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .clear
         
+        let barButtonAppearance = UIBarButtonItemAppearance()
+        barButtonAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor.clear,
+            .font: UIFont.systemFont(ofSize: .zero)
+        ]
         
+        let backButtonImage = UIImage(named: "left")?.withRenderingMode(.alwaysTemplate)
+        appearance.setBackIndicatorImage(backButtonImage, transitionMaskImage: backButtonImage)
+        
+        navigationController.navigationBar.tintColor = .neutral
+        navigationController.navigationBar.standardAppearance = appearance
+        navigationController.navigationBar.scrollEdgeAppearance = appearance
     }
 }
