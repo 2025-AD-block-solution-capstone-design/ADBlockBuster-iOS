984ca10c38118adeb0bdf217ab744d161a07b52e
Jiyong <hickerd@sch.ac.kr>
2025-05-15 03:45:40 +0900

    design: Add Navigation Style

--- AD-BlockBuster/Source/MainFlow/Home/View/ADHomeViewController.swift
+++ AD-BlockBuster/Source/MainFlow/Home/View/ADHomeViewController.swift
@@ -8,11 +8,48 @@
 import UIKit
 
 final class ADHomeViewController: BaseViewController<ADHomeView> {
-    // MARK: - Properties
     weak var coordinator: ADHomeCoordinator?
-
-    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+}
+
+private extension ADHomeViewController {
+    @objc
+    func rightBarButtonTapped() {
+        
+    }
+    
+    func setupNavigationBar() {
+        let leftBarButtonImage = UIImage(named: LayoutConstants.leftImageName)
+        let rightBarButtonImage = UIImage(named: LayoutConstants.rightImageName)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: leftBarButtonImage,
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: rightBarButtonImage,
+            style: .plain,
+            target: self,
+            action: #selector(rightBarButtonTapped)
+        )
+    }
+}
+
+private extension ADHomeViewController {
+    enum LayoutConstants {
+        static let leftImageName: String = "icons/tornado"
+        static let rightImageName: String = "icons/setting"
     }
 }
